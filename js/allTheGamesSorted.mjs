5eac77d3df087447d39c77dea72ec6f85f639f03
HelChris <111705707+HelChris@users.noreply.github.com>
2024-03-11 21:56:57 +0100

    added gamecards to index

--- js/allTheGamesSorted.mjs
+++ js/allTheGamesSorted.mjs
@@ -3,25 +3,27 @@ import { url } from "./constants.mjs";
 
 //switch function
 const switchTheme = () => {
-	// get root element and data-theme value
+	// get root element and data-theme value::
 	const rootElm = document.documentElement;
 	let dataTheme = rootElm.getAttribute("data-theme"),
 		newTheme;
 
 	newTheme = dataTheme === "light" ? "dark" : "light";
 
-	//set the new HTML attribute
+	//set the new HTML attribute::
 	rootElm.setAttribute("data-theme", newTheme);
 
-	//Set the new local storage item
+	//Set the new local storage item::
 	localStorage.setItem("theme", newTheme);
 };
 
-//Add event listener for the theme switcher
+//Add event listener for the theme switcher::
 document
 	.querySelector("#theme-switcher")
 	.addEventListener("click", switchTheme);
 
+
+//all the games sorted by category::
 async function getGames() {
 	try {
 		const response = await fetch(url);
